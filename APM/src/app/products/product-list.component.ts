2c1b648a5a229e3ab2d1aab62c4ad18a41a2a0a9
tf016851 <tim.fite@cerner.com>
2017-09-25 09:58:44 -0500

    Adding my work

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,57 @@
+import { IProduct } from './product';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+
+@Component({
+  // selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = true;
+  filteredProducts: IProduct[];
+  products: IProduct[];
+  errorMessage: string;
+
+  _listFilter: string;
+
+  constructor(private _productService: ProductService) {
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+    this._productService.getProducts()
+      .subscribe(products => {
+                    this.products = products;
+                    this.filteredProducts = this.products;
+                 },
+                 error => this.errorMessage = <any>error);
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+              product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+}
